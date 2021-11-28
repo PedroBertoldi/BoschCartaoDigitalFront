1cf03412200b3b9c7f5d23236be05fd124cca675
RodrigoDMusha <rodrigo.musha@gmail.com>
2021-11-28 00:10:24 -0300

    header and footer

--- src/app/area-publica/area-publica-routing.module.ts
+++ src/app/area-publica/area-publica-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AreaPublicaLoginComponent } from './area-publica-login/area-publica-login.component';
 import { AreaPublicaComponent } from './area-publica.component';
 
-const routes: Routes = [{ path: '', component: AreaPublicaComponent }];
+const routes: Routes = [{ path: 'meus-beneficios', component: AreaPublicaComponent },
+                        { path: '', component: AreaPublicaLoginComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/area-publica/area-publica.module.ts
+++ src/app/area-publica/area-publica.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { AreaPublicaRoutingModule } from './area-publica-routing.module';
 import { AreaPublicaComponent } from './area-publica.component';
+import { AreaPublicaLoginComponent } from './area-publica-login/area-publica-login.component';
 
 
 @NgModule({
   declarations: [
-    AreaPublicaComponent
+    AreaPublicaComponent,
+    AreaPublicaLoginComponent
   ],
   imports: [
     CommonModule,
