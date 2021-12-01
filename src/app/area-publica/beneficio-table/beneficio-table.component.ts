e0b565d6dc8d3f355d7c75bc581086c8e3c102c9
RodrigoDMusha <rodrigo.musha@gmail.com>
2021-12-01 16:15:56 -0300

    dinamic loading beneficios and  indicacoes

--- src/app/area-publica/beneficio-table/beneficio-table.component.ts
+++ src/app/area-publica/beneficio-table/beneficio-table.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-beneficio-table',
@@ -6,10 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./beneficio-table.component.css']
 })
 export class BeneficioTableComponent implements OnInit {
-
+  @Input() data!: any[];
   constructor() { }
 
   ngOnInit(): void {
+    
   }
 
 }
