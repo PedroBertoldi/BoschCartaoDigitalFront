317ae91786cf40b7be93c771839c2176c54935ac
AnaCarla <anacarla_caldelgiac@hotmail.com>
2021-12-02 02:44:50 -0300

    criado mock validacao

---
+++ src/app/area-operacional/area-operacional-validacao/area-operacional-validacao.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-area-operacional-validacao',
+  templateUrl: './area-operacional-validacao.component.html',
+  styleUrls: ['./area-operacional-validacao.component.css']
+})
+export class AreaOperacionalValidacaoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/area-operacional/area-operacional.module.ts
+++ src/app/area-operacional/area-operacional.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { AreaOperacionalRoutingModule } from './area-operacional-routing.module';
 import { AreaOperacionalComponent } from './area-operacional.component';
-
+import { AreaOperacionalValidacaoComponent } from './area-operacional-validacao/area-operacional-validacao.component';
 
 @NgModule({
   declarations: [
-    AreaOperacionalComponent
+    AreaOperacionalComponent,
+    AreaOperacionalValidacaoComponent
   ],
   imports: [
     CommonModule,
