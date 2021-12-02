d5b180eeec393f1d59282d92218be02694adf491
Bruno Canale <b.canalemedeiros@gmail.com>
2021-12-02 11:34:16 -0300

    inserir coluna ativo

--- src/app/area-administrativa/area-administrativa-consulta-evento/area-administrativa-consulta-evento.component.ts
+++ src/app/area-administrativa/area-administrativa-consulta-evento/area-administrativa-consulta-evento.component.ts
@@ -13,24 +13,28 @@ export class AreaAdministrativaConsultaEventoComponent implements OnInit {
       nome: 'Final de ano 2021',
       inicio: '19-12-2021',
       fim: '20-12-2021',
+      ativo: true,
     },
     {
       id: 2,
       nome: 'Páscoa 2022',
       inicio: '17-04-2022',
       fim: '18-04-2022',
+      ativo: false,
     },
     {
       id: 3,
       nome: 'Festa Junina 2022',
       inicio: '23-06-2022',
       fim: '24-06-2022',
+      ativo: false,
     },
     {
       id: 4,
       nome: 'Carnaval 2022',
       inicio: '25-02-2022',
       fim: '26-02-2022',
+      ativo: false,
     },
   ]
 
