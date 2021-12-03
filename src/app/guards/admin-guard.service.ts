8300280dc501dd3d9d8acaf271e6a224576847e2
RodrigoDMusha <rodrigo.musha@gmail.com>
2021-12-03 15:15:13 -0300

    admin guard and access control for admin area

---
+++ src/app/guards/admin-guard.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthenticationService } from '../services/authentication.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminGuardService implements CanActivate{
+
+  constructor(private auth: AuthenticationService, private router: Router) { }
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+        const currentUser = this.auth.getUser();
+        if (currentUser) {
+          console.log(currentUser.cargos)
+          if(currentUser.cargos.includes('HRL')){
+            return true;
+          } else{
+            return false;
+          }
+        }
+        this.router.navigate(['/admin']);
+        return false;
+    }
+}
