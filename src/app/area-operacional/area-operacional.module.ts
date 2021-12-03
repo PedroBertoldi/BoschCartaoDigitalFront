f4679770a2bb16fd64e633eb34356db22697a560
AnaCarla <anacarla_caldelgiac@hotmail.com>
2021-12-03 01:57:44 -0300

    adicionado campos de cpf e edv

--- src/app/area-operacional/area-operacional.module.ts
+++ src/app/area-operacional/area-operacional.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 
 import { AreaOperacionalRoutingModule } from './area-operacional-routing.module';
 import { AreaOperacionalComponent } from './area-operacional.component';
@@ -16,7 +17,8 @@ import { BotaoValidacaoComponent } from './area-operacional-validacao/botao-vali
   imports: [
     CommonModule,
     AreaOperacionalRoutingModule,
-    MatButtonModule
+    MatButtonModule,
+    MatInputModule
   ]
 })
 export class AreaOperacionalModule { }
