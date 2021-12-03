c171aaf459ad3430bdde04d608607eb6ddf5e406
AnaCarla <anacarla_caldelgiac@hotmail.com>
2021-12-03 00:16:13 -0300

    adicionado click no botão para mostrar input

--- src/app/area-operacional/area-operacional-validacao/area-operacional-validacao.component.ts
+++ src/app/area-operacional/area-operacional-validacao/area-operacional-validacao.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AreaOperacionalValidacaoComponent implements OnInit {
 
+  mostraCampoCPF: string = "esconder";
+  mostraCampoEDV: string = "esconder";
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  mostrarDiv(resposta: any): void {
+    if (resposta === 'selecionadoCPF') {
+      this.mostraCampoCPF = "mostrar";
+      this.mostraCampoEDV = "esconder";
+    } else if (resposta === 'selecionadoEDV') {
+      this.mostraCampoEDV = "mostrar";
+      this.mostraCampoCPF = "esconder";
+    }
+  }
 }
--- src/app/area-operacional/area-operacional-validacao/botao-validacao/botao-validacao.component.ts
+++ src/app/area-operacional/area-operacional-validacao/botao-validacao/botao-validacao.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-botao-validacao',
@@ -7,8 +7,10 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class BotaoValidacaoComponent implements OnInit {
 
-  @Input() variante!: 'ValidacaoCracha' | 'ValidacaoCpf' | 'ValidacaoEdv'
+  @Input() variante!: 'ValidacaoCracha' | 'ValidacaoCPF' | 'ValidacaoEDV'
 
+  @Output() buttonClick = new EventEmitter();
+  
   constructor() { }
 
   ngOnInit(): void {
