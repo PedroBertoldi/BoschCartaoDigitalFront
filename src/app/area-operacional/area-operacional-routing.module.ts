56f0c70e66c5ea71482b4af7833dc297b9fb656b
RodrigoDMusha <rodrigo.musha@gmail.com>
2021-12-04 15:21:48 -0300

    login adaptation and route config

--- src/app/area-operacional/area-operacional-routing.module.ts
+++ src/app/area-operacional/area-operacional-routing.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AreaOperacionalComponent } from './area-operacional.component';
 import { AreaOperacionalValidacaoComponent } from './area-operacional-validacao/area-operacional-validacao.component';
+import { LoginComponent } from '../common/login/login.component';
 
 const routes: Routes = [
-  { path: '', component: AreaOperacionalComponent },
+  { path: '', component: LoginComponent },
+  { path: 'retirar/:cpf', component: AreaOperacionalComponent },
+  { path: 'retirar/:edv', component: AreaOperacionalComponent },
   { path: 'validacao', component: AreaOperacionalValidacaoComponent },
 ];
 
