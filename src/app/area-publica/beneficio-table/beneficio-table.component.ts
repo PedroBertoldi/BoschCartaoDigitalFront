904135893005a9144e2e504cd107b064caedb5de
RodrigoDMusha <rodrigo.musha@gmail.com>
2021-12-05 19:45:29 -0300

    quantidade in public area

--- src/app/area-publica/beneficio-table/beneficio-table.component.ts
+++ src/app/area-publica/beneficio-table/beneficio-table.component.ts
@@ -7,10 +7,33 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class BeneficioTableComponent implements OnInit {
   @Input() data!: any[];
+  direitos: any =[];
   constructor() { }
 
   ngOnInit(): void {
-    
   }
+
+  ngOnChanges():void{
+    if(this.data){
+      console.log(this.data);
+      
+      this.data.forEach(direitoIndividual => {
+        let rep = false;
+        this.direitos.forEach((direito:any) => {
+          if(direito.beneficio.id == direitoIndividual.beneficio.id){
+            direito.quantidade ++;
+            rep =true;
+          }
+        });
+        if(rep == false){
+          direitoIndividual.quantidade =1;
+          this.direitos.push(direitoIndividual);
+        }
+
+      });
+
+    }
+  }
+  
 
 }
