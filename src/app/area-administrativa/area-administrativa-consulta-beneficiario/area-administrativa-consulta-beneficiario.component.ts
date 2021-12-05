7d1cb0c3d1669f664bfd0d3f5352836eb1146b32
RodrigoDMusha <rodrigo.musha@gmail.com>
2021-12-05 19:30:43 -0300

    quantity in beneficios in beneficiarios list

--- src/app/area-administrativa/area-administrativa-consulta-beneficiario/area-administrativa-consulta-beneficiario.component.ts
+++ src/app/area-administrativa/area-administrativa-consulta-beneficiario/area-administrativa-consulta-beneficiario.component.ts
@@ -14,9 +14,9 @@ export class AreaAdministrativaConsultaBeneficiarioComponent implements OnInit {
 
   idEventoFromRoute!: number;
 
-  beneficiarios!: any[]
+  beneficiarios!: any
 
-  buscaBeneficiarios!:any[]
+  buscaBeneficiarios!:any
 
   constructor(private route: ActivatedRoute, private beneficiarioService:BeneficiarioService) { }
 
@@ -24,8 +24,26 @@ export class AreaAdministrativaConsultaBeneficiarioComponent implements OnInit {
     const routeParams = this.route.snapshot.paramMap;
     this.idEventoFromRoute = Number(routeParams.get('idEvento'));
     this.beneficiarioService.getBeneficiarios(this.idEventoFromRoute).pipe(first()).subscribe(
-      data=>{
+      (data:any)=>{
         this.beneficiarios= data.colaboradoresDireitos;
+        this.beneficiarios.forEach((beneficiario:any) => {
+          let direitos: any[] = [];
+          beneficiario.direitos.forEach((direitoUnitario: any) => {
+            let rep = false;
+            direitos.forEach((direito:any) => {
+              if(direitoUnitario.beneficio.id == direito.beneficio.id){
+                direito.quantidade++;
+                rep = true;
+              }
+            });
+            if(!rep){
+              direitoUnitario.quantidade =1;
+              direitos.push(direitoUnitario);
+            }
+          });
+          beneficiario.direitos = direitos;
+        });
+        
         this.buscaBeneficiarios = this.beneficiarios
       }
     )
