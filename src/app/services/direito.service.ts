813abe1b26f1656ad2a8e12beaeec00e4e0ffa3b
PedroBertoldi <henrique_pedro900@hotmail.com>
2021-12-06 19:51:28 -0300

    Correção URL

--- src/app/services/direito.service.ts
+++ src/app/services/direito.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,7 @@ export class DireitoService {
   constructor(private http: HttpClient) { }
 
   getDireitos(cpf:string, nascimento: string){
-    return this.http.get<any>('http://localhost:5000/api/AreaPublica/buscar-direitos', {params:{ Cpf: cpf, DataNascimento: nascimento}} );
+    return this.http.get<any>(environment.webApi + '/api/AreaPublica/buscar-direitos', {params:{ Cpf: cpf, DataNascimento: nascimento}} );
   }
   
 }
--- src/app/services/indicacao.service.ts
+++ src/app/services/indicacao.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -9,15 +10,15 @@ export class IndicacaoService {
   constructor(private http: HttpClient) { }
 
   getColaborador(edv:string){
-    return this.http.get<any>('http://localhost:5000/api/AreaPublica/buscar-colaborador/'+ edv);
+    return this.http.get<any>(environment.webApi + '/api/AreaPublica/buscar-colaborador/'+ edv);
   }
 
   getIndicado(id:number, nascimento: string){
-    return this.http.get<any>('http://localhost:5000/api/AreaPublica/buscar-indicado',{params:{"ColaboradorId":id,DataNascimento:nascimento}} );
+    return this.http.get<any>(environment.webApi + '/api/AreaPublica/buscar-indicado',{params:{"ColaboradorId":id,DataNascimento:nascimento}} );
   }
 
   removeIndicar(colaboradorId: number){
-    return this.http.put<any>('http://localhost:5000/api/AreaPublica/remover-indicacoes',
+    return this.http.put<any>(environment.webApi + '/api/AreaPublica/remover-indicacoes',
     {
       colaboradorId: colaboradorId,
       direitosId:[]
@@ -25,7 +26,7 @@ export class IndicacaoService {
   }
 
   indicar(colaboradorId: number, nomeCompleto:string, cpf:string, edv:string){
-     return this.http.put<any>('http://localhost:5000/api/AreaPublica/indicar-pessoa', 
+     return this.http.put<any>(environment.webApi + '/api/AreaPublica/indicar-pessoa', 
      {
       colaboradorId: colaboradorId,
       nomeCompleto: nomeCompleto,
